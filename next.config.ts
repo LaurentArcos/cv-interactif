78bd27861a170643cf3ef24d4c7371cf3d13351d
LaurentArcos <laurent.arcos@gmail.com>
2025-01-12 17:18:32 +0100

    modif

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@
 const nextConfig = {
   output: 'standalone',
   basePath: '/cv',
-  assetPrefix: '/cv/',
+  assetPrefix: '/cv',
   images: {
     loader: 'default',
-    path: '/cv/_next/image',
-    domains: ['www.laurentarcos.fr'],
+    path: '/cv/_next/image/',
   },
 };
 
